e704d6a0a2108c7736c2a5390bc661ae9053788a
nandigum yeshwanth <nandigumyeshwanth907@gmail.com>
2025-07-10 10:15:25 +0000

    problem not solved

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
+
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
+import { getAuth, type Auth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +17,18 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
+let app: FirebaseApp;
+let auth: Auth;
+
+if (firebaseConfig.apiKey) {
+    app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+    auth = getAuth(app);
+} else {
+    console.error("Firebase API key is not set. Firebase will not be initialized.");
+    // Provide mock objects or handle the uninitialized state gracefully
+    app = {} as FirebaseApp;
+    auth = {} as Auth;
+}
+
 
 export { app, auth };
